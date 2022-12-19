af0dd290c6775e333e0e85d76f17f1f71fcebcd1
ican-graphics-01 <lankaican@gmail.com>
2022-12-19 14:09:27 +0530

    how to use a foor loop

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
     <!-- <h1>Hello from home component</h1> -->
-    <h1>Event</h1>
+    <!-- <h1 v-if="show">Conditional Rendering</h1> -->
+    <h1>For loop in vue.js</h1>
+    <table border="1px">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+        </tr>
+
+        <tr v-for="user in users" :key="user.id">
+            <td>{{user.id}}</td>
+            <td>{{user.name}}</td>
+            <td>{{user.email}}</td>
+        </tr>
+
+    </table>
+    <!-- <h1 v-else>else side</h1> -->
+    <!-- <button v-on:click="display">Toggle</button> -->
     <!-- <h1>{{ data }}</h1>
     <h1>{{ msg }}</h1> -->
 
-    <button v-on:mouseover ="hello('data')">Call Hello functiomn</button>
+    <!-- <button v-on:mouseover ="hello('data')">Call Hello functiomn</button> -->
 
   </div>
 </template>
@@ -17,11 +34,27 @@ export default {
         data:String,
         msg:String
     },
-    methods: {
-        hello(item) {
-            console.log(item)
+    // methods: {
+    //     hello(item) {
+    //         console.log(item)
+    //     }
+    // }
+    data(){
+        return{
+           users: [
+                {id:1, name:'Gimhan', email:'gimhan@gmail.com'},
+                {id:2, name:'Pabasara', email:'pabasara@gmail.com'},
+                {id:3, name:'Munasinghe', email:'munasinghe@gmail.com'},
+                {id:4, name:'Wasudewa', email:'wasudewa@gmail.com'},
+
+            ]
         }
-    }
+    },
+    // methods:{
+    //     display(){
+    //         this.show=!this.show
+    //     }
+    // }
 }
 </script>
 
